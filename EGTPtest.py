b537abcb21d254dc43ea0cea73251b59db0381d4
zooko <zooko>
2002-03-13 21:27:13 +0000

    * there is only one unit test

--- EGTPtest.py
+++ EGTPtest.py
@@ -6,7 +6,7 @@
 #    GNU Lesser General Public License v2.1.
 #    See the file COPYING or visit http://www.gnu.org/ for details.
 #
-__cvsid = '$Id: EGTPtest.py,v 1.7 2002/03/13 21:24:31 zooko Exp $'
+__cvsid = '$Id: EGTPtest.py,v 1.8 2002/03/13 21:27:13 zooko Exp $'
 
 # standard Python modules
 import threading, types
@@ -90,40 +90,6 @@ def _help_test(finishedflag, numsuccessesh, lm, dm, name="a test"):
     # Have the second node ping the first, using only the first's id.
     n2.send(CommStrat.addr_to_id(n1.get_address()), mtype="ping", msg="hello there, you crazy listener!")
 
-def test_0(finishedflag, numsuccessesh):
-    """
-    This is testing obsolete behavior, but egtp doesn't yet pass test_1(), which tests the new behavior...
-    """
-    start = timer.time()
-
-    localLM = LocalLookupMan()
-    localDM = LocalDiscoveryMan()
-
-    # make a listener
-    l = Node.Node(allownonrouteableip=true, lookupman=localLM, discoveryman=localDM) # `allownonrouteableip' means: tell it to do TCP even if you are not routeable from the real Internet (for testing)
-    
-    # get its "EGTP address" record, which includes its public key and a set of protocols+addresses by which it can be reached
-    lsaddr = l.get_address()
-
-    lsid = CommStrat.addr_to_id(lsaddr)
-    localLM.publish(lsid, lsaddr)
-
-    # set a handler func: if any messages come in with message type "ping", the EGTP Node will call this function.
-    def l_ping_handler(sender, msg, finishedflag=finishedflag, numsuccessesh=numsuccessesh, start=start):
-        debugprint("test_0: passed in %s seconds: Got a message from %s.  The message says: %s\n", args=("%0.1f" % (timer.time() - start), sender, msg,), v=0)
-        numsuccessesh[0] += 1
-        finishedflag.set()
-
-    l.set_handler_func(mtype="ping", handler_func=l_ping_handler)
-
-    # make a second EGTP Node for sending
-    s = Node.Node(lookupman=localLM, discoveryman=localDM)
-
-    # have the 2nd node ping the first
-    # debugprint("test_0: about to send!\n", v=0)
-    s.send(lsid, mtype="ping", msg="hello there, number one!")
-    # debugprint("test_0: sent!\n", v=0)
-
 def test_1(finishedflag, numsuccessesh):
     localLM = LocalLookupMan()
     localDM = LocalDiscoveryMan()
@@ -157,5 +123,5 @@ def runalltests(tests, expectedfailures=0):
 
     assert numsuccessesh[0] == len(tests), "not all tests passed: num successes: %s, num failures: %s" % (numsuccessesh[0], map(lambda x: x[0], filter(lambda x: not x[1].isSet(), ts)),)
 
-runalltests((test_0, test_1,), expectedfailures=0)
+runalltests((test_1,), expectedfailures=0)
 
